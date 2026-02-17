cd47804f686685374c1a8e8d9cf29672ab8ec576
johnsulf <erlendjohns@gmail.com>
2026-02-17 14:00:24 +0100

    adds deploy info to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://erlendjohnsen.github.io',
+  base: '/team',
   vite: {
     plugins: [tailwindcss()]
   }
