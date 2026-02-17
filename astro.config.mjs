e7ba3943e069dd971ddf4447ed847b6c1765b1d0
johnsulf <erlendjohns@gmail.com>
2026-02-17 14:07:17 +0100

    adds trailing slash in routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://erlendjohnsen.github.io',
+  site: 'https://johnsulf.github.io',
   base: '/team',
   vite: {
     plugins: [tailwindcss()],
